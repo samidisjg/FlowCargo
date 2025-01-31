673f8010f452cc2dc9e22f08fa7891877604f4e1
Samidi Jayawickrama <107723505+samidisjg@users.noreply.github.com>
2025-01-31 12:28:19 +0530

    feat:InterserviceCommuncation added. Order and Inventory

--- order-service1/src/main/java/com/sami/microservices/order/service/OrderService.java
+++ order-service1/src/main/java/com/sami/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.sami.microservices.order.service;
 
+import com.sami.microservices.order.client.InventoryClient;
 import com.sami.microservices.order.model.Order;
 import com.sami.microservices.order.repository.OrderRepository;
 import com.sami.microservices.order.request.OrderRequest;
@@ -12,18 +13,24 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        //map OrderRequest to Order Object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+       var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        //save order to OrderRepository
-        orderRepository.save(order);
+       if (isProductInStock) {
+           //map OrderRequest to Order Object
+           Order order = new Order();
+           order.setOrderNumber(UUID.randomUUID().toString());
+           order.setPrice(orderRequest.price());
+           order.setSkuCode(orderRequest.skuCode());
+           order.setQuantity(orderRequest.quantity());
 
+           //save order to OrderRepository
+           orderRepository.save(order);
 
+       }else {
+           throw new RuntimeException("Product with sku code " + orderRequest.skuCode() + " is not in stock");
+       }
     }
 }
